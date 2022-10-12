6b6dd12284cf766ff71c93a90dc5313f7bc71c47
Ryan-Hinds <101537819+Ryan-Hinds@users.noreply.github.com>
2022-10-12 14:03:30 -0600

    changed to a number game

--- script.js
+++ script.js
@@ -1,34 +1,70 @@
-"use strict"
+"use strict";
 
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const closeModalBTN = document.querySelector(".close-modal");
-const showModalBTN = document.querySelectorAll(".show-modal");
+// Selecting Elements
+const player0Elem = document.querySelector(".player--0");
+const player1Elem = document.querySelector(".player--1");
+const score0Elem = document.querySelector("#score--0");
+const player0Score = document.getElementById("current--0");
+const score1Elem = document.getElementById("score--1");
+const player1Score = document.getElementById("current--1");
 
-for (let i = 0; i < showModalBTN.length; i++)
+// Dice and Button Elements
+const diceElem = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+
+// Starting conditions
+score0Elem.textContent = 0;
+score1Elem.textContent = 0;
+diceElem.classList.add("hidden");
+
+function switchPlayer()
 {
-    console.log(showModalBTN[i].addEventListener("click", function()
-    {
-        modal.classList.remove("hidden");
-        overlay.classList.remove("hidden");
-    }));
+    // Switch to next player.
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0Elem.classList.toggle("player--active");
+    player1Elem.classList.toggle("player--active");
 }
 
-closeModalBTN.addEventListener("click", function()
+// Rolling dice functionality
+btnRoll.addEventListener("click", function()
 {
-    closeModal();
+    // 1: Generate random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    // 2: Display dice
+    diceElem.classList.remove("hidden");
+    diceElem.src = `dice-${dice}.png`;
+    // 3: Check for roll 1: if true, switch to next player
+    if (dice !== 1)
+    {
+        // Add dice number to current score.
+        currentScore += dice;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    }
+    else
+    {
+        switchPlayer();
+    }
 });
 
-document.addEventListener("keydown", function(e)
-{
-    if (e.key === "Escape")
-    {
-        closeModal();
-    }
-})
+btnHold.addEventListener("click", holdButtonFunctionality);
 
-function closeModal()
+function holdButtonFunctionality()
 {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+    if (scores[activePlayer] >= 100)
+    {
+        document.querySelector(`.player--${activePlayer}`).classList.add("player--winner");
+        document.querySelector(`.player--${activePlayer}`).classList.remove("player--active");
+    }
+    switchPlayer();
 }
